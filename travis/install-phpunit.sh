526acdc469e5ab3916a92505eb63a5178b3e7bc5
Alexander Gesinn <alexander.gesinn@gesinn.it>
2021-02-18 19:43:35 +0100

    added install-phpunit.sh

---
+++ travis/install-phpunit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+BASE_PATH=$(pwd)
+MW_INSTALL_PATH=$BASE_PATH/../mw
+
+cd $MW_INSTALL_PATH
+
+if [ "$PHPUNIT" != "" ]
+then
+		composer require 'phpunit/phpunit='$PHPUNIT --update-with-dependencies
+fi
