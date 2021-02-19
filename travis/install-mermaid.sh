f1bd139a9a606c6a4975232d6e5ee6334bf5b5ba
gesinn-it <github@gesinn.it>
2021-02-19 18:12:40 +0100

    enable only if installed

--- travis/install-mermaid.sh
+++ travis/install-mermaid.sh
@@ -4,11 +4,12 @@ set -ex
 BASE_PATH=$(pwd)
 MW_INSTALL_PATH=$BASE_PATH/../mw
 
-# Install
 cd $MW_INSTALL_PATH
-if [ "$MERMAID" != "" ]; then
-  composer require 'mediawiki/mermaid='$MERMAID --update-with-dependencies
-fi
 
-# Configure
-echo 'wfLoadExtension( "Mermaid" );' >> LocalSettings.php
+if [ "$MERMAID" != "" ]; then
+  # Install
+  composer require 'mediawiki/mermaid='$MERMAID --update-with-dependencies
+
+  # Configure
+  echo 'wfLoadExtension( "Mermaid" );' >> LocalSettings.php
+fi
