47ce67eda8c6fd844a2497539b7a26a710c1bc4b
gesinn-it <github@gesinn.it>
2021-02-23 10:04:55 +0100

    added option to set $wgLanguageCode via SITELANG parameter

--- travis/install-mediawiki.sh
+++ travis/install-mediawiki.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-# Download
+## Download
 cd ..
 wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
 tar -zxf $MW.tar.gz
 mv mediawiki-$MW mw
 
-# Install
+## Install
 cd mw
 if [ -f composer.json ]; then
   composer install
@@ -20,9 +20,15 @@ else
   php maintenance/install.php --dbtype $DB --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass AdminPassword TravisWiki admin --scriptpath /TravisWiki
 fi
 
-# Configure
+## Configure
 echo 'error_reporting(E_ALL|E_STRICT);' >> LocalSettings.php
 echo 'ini_set("display_errors", 1);' >> LocalSettings.php
 echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
 echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
 echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
+
+# Site language
+if [ "$SITELANG" != "" ]
+then
+  echo '$wgLanguageCode = "'$SITELANG'";' >> LocalSettings.php
+fi
